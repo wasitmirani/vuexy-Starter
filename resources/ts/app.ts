3a5aae53ce0aa4661e2aff01c1601ad02d87edbd
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2022-08-23 18:02:18 +0500

    changes

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -8,14 +8,37 @@ import MasterLayout from "./vue/backend/layouts/MasterLayoutComponent.vue"
 import router from "./router";
 import Vue3ColorPicker    from "vue3-colorpicker";
 import "vue3-colorpicker/style.css";
+import Notifications from '@kyvg/vue3-notification';
+
 
 
 
 const app =createApp({
+    methods:{
+        alertNotify( status:any, title:String, type:String, response:any){
+            switch (status) {
+
+                case 200:
+                    this.$notify({title: title,type:type,text: response.message});
+                break;
+                case 201:
+                    this.$notify({title: title,type:type,text: response.message});
+                break;
+                case 422:
+                    this.$notify({title: `Oops, Unprocessable Entity ${status} Error! `,type:type,text: response.message});
+                break;
+                case 500:
+                    this.$notify({title: `Oops, Something Went Wrong ${status} Error! `,type:type,text: response.message});
+                    break;
+            }
+        },
+    },
     mounted(){
         console.log("app mounted");
     }
 });
 app.component('master-layout',MasterLayout);
 app.use(Vue3ColorPicker);
+app.use(Notifications)
+
 app.use(router).mount('#app');
